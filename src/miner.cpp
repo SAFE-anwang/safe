d1f97992b851d896e8ac0b67669372ee7c192b1e
xiangjie256329 <xiangjie256329@163.com>
2018-12-24 15:54:27 +0800

    first index no check

--- src/miner.cpp
+++ src/miner.cpp
@@ -693,7 +693,7 @@ static void ConsensusUseSPos(const CChainParams& chainparams,CConnman& connman,C
         }
 
         int64_t nActualTimeMillisInterval = std::abs(nNextTime - nCurrTime);
-        if(nActualTimeMillisInterval > nIntervalMS && nNextTime!=0)
+        if(nActualTimeMillisInterval > nIntervalMS && nNextTime!=0 && index != 0)
         {
             if(nNewBlockHeight != nWaitBlockHeight)
                 LogPrintf("SPOS_Warning:nActualTimeMillisInterval(%d) big than nIntervalMS(%d)\n",nActualTimeMillisInterval,nIntervalMS);
