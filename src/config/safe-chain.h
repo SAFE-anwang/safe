fc0ce1ba596a437843d6bf19e5e943d237b60765
github1000 <50705646@qq.com>
2019-06-24 16:19:18 +0800

    fix crash bug

--- src/config/safe-chain.h
+++ src/config/safe-chain.h
@@ -14,6 +14,7 @@
 #define SCN__test       20
 
 ////////////////////////////////////////////////////////////
+#define SAFE_CHAIN_NAME dev
 
 #if !defined(SAFE_CHAIN_NAME)
 #pragma message("use default safe-chain-name: main")
