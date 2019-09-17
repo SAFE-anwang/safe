627bbab03d45dd0a92f8315d58e216c69105d0c8
shuqijkx <l>
2019-09-17 14:54:49 +0800

    Code error

--- src/txdb.h
+++ src/txdb.h
@@ -162,10 +162,10 @@ public:
     bool Is_Exists_MasternodePayee_Key(const std::string& strPubKeyCollateralAddress);
 
     bool Write_MasternodePayee_DMN_Index(const std::string& strPubKeyCollateralAddress, const CMasternodePayee_DMN_IndexValue& value);
-    bool Erase_MasternodePayee_DMN_Index(const string &strPubKeyCollateralAddress);
-    bool Read_MasternodePayee_DMN_Index(const string &strPubKeyCollateralAddress, CMasternodePayee_DMN_IndexValue &value);
-    bool Read_MasternodePayee_DMN_Index(std::map<string, CMasternodePayee_DMN_IndexValue> &mapPayeeInfo);
-    bool Is_Exists_MasternodePayee_DMN_Key(const string &strPubKeyCollateralAddress);
+    bool Erase_MasternodePayee_DMN_Index(const std::stringstring &strPubKeyCollateralAddress);
+    bool Read_MasternodePayee_DMN_Index(const std::stringstring &strPubKeyCollateralAddress, CMasternodePayee_DMN_IndexValue &value);
+    bool Read_MasternodePayee_DMN_Index(std::map<std::string, CMasternodePayee_DMN_IndexValue> &mapPayeeInfo);
+    bool Is_Exists_MasternodePayee_DMN_Key(const std::string &strPubKeyCollateralAddress);
 
     bool Write_LocalStartSavePayeeHeight_Index(const int& nHeight);
     bool Read_LocalStartSavePayeeHeight_Index(int& nHeight);
